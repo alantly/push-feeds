3ff93be654d9c01567a774726a5258cc5622130f
Alan <alanly211@gmail.com>
2016-01-29 08:16:25 -0800

    made normalization on url before saving for feed table

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,10 @@
 class Feed < ActiveRecord::Base
   has_and_belongs_to_many :users
+  before_save :normalize_url
+
+  private
+
+  def normalize_url
+    self.url = /^https?:\/\/(.*)?\//.match(url)[1]
+  end
 end
