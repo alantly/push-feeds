0646cc92fe87a31c7332be2cbc7c1b6036fe1a4b
Alan <alanly211@gmail.com>
2016-01-30 18:21:53 -0800

    made validation to check if url not null and removed protocol on uri

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,10 +1,26 @@
 class Feed < ActiveRecord::Base
   has_and_belongs_to_many :users
   before_save :normalize_url
+  validates :url_valid_uri, :url, presence: true
 
   private
 
+  def url_valid_uri
+    begin
+      URI.parse(url)
+    rescue URI::BadURIError
+      errors.add :url, "is a bad URI."
+    rescue URI::InvalidURIError
+      errors.add :url, "is an invalid URI."
+    end
+  end
+
   def normalize_url
-    self.url = /^https?:\/\/(.*)?\//.match(url)[1]
+    resource = /^(?:https?:\/\/)?(.+)\/$/.match(self.url)
+    unless resource.nil?
+      self.url = resource[1]
+    else
+      self.url = /^(?:https?:\/\/)?(.+)$/.match(self.url)[1]
+    end
   end
 end
