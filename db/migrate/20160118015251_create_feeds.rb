3c3e586b1ecc72057a41ec331fe4b14614f2b150
Alan <alanly211@gmail.com>
2016-02-03 17:56:37 -0800

    added some constraints to db

--- db/migrate/20160118015251_create_feeds.rb
+++ db/migrate/20160118015251_create_feeds.rb
@@ -1,8 +1,8 @@
 class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
-      t.string :url
-      t.string :secret
+      t.string :url, :unique => true
+      t.string :secret, :unique => true
 
       t.timestamps
     end
