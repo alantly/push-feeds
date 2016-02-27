42ec4d716d28d61f3e82047bfc4d40c7d780f597
Alan <alanly211@gmail.com>
2016-02-26 17:34:22 -0800

    fixed event.waituntil issue and added ability to go to a url on click

--- app/assets/javascripts/notification/sw.js
+++ app/assets/javascripts/notification/sw.js
@@ -11,74 +11,49 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('push', function(event) {
   console.log('Push message', event);
-  fetch("/feeds/updated", {
-    credentials: 'same-origin',
-    mode: 'same-origin'
-  }).then(function(response) {
-    if (!response.ok) {
-      return;
-    }
-    response.json().then(function(data) {
-      event.waitUntil(
-        showNotification(data.title, data.message, "google.com")
-      );
-    });
-  }).catch(function(error) {
-    console.log("There was an error", error);
-    showNotification("Push-Feeds", "A Wild Notification has appeared!", "www.google.com");
-  });
+  event.waitUntil(
+    fetch("/feeds/updated", {
+      credentials: 'same-origin',
+      mode: 'same-origin'
+    }).then(function(response) {
+      if (!response.ok) {
+        return;
+      }
+      response.json().then(function(data) {
+        showNotification(data.title, data.message, data.url);
+      });
+    }).catch(function(error) {
+      console.log("There was an error", error);
+      showNotification("Push-Feeds","A wild notification has appeared!","https://www.google.com");
+    })
+  );
 });
 
 function showNotification(title, message, url) {
-    self.registration.showNotification(title, {
-      body: message,
-      // icon: 'images/icon.png',
-    });
-}
 
-function handleNotificationClick(event) {
-    console.log('Notification click: tag ', event.notification.tag);
-    debugger;
-    event.notification.close();
-    var url = 'https://google.com';
-    event.waitUntil(
-        clients.matchAll({
-            type: 'window'
-        })
-        .then(function(windowClients) {
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
-}
+  self.addEventListener('notificationclick', function(event) {
+      console.log('Notification click: tag ', event.notification.tag);
+      event.notification.close();
+      event.waitUntil(
+          clients.matchAll({
+              type: 'window'
+          })
+          .then(function(windowClients) {
+              for (var i = 0; i < windowClients.length; i++) {
+                  var client = windowClients[i];
+                  if (client.url === url && 'focus' in client) {
+                      return client.focus();
+                  }
+              }
+              if (clients.openWindow) {
+                  return clients.openWindow(url);
+              }
+          })
+      );
+  });
 
-//
-// self.addEventListener('notificationclick', function(event) {
-//     console.log('Notification click: tag ', event.notification.tag);
-//     debugger;
-//     event.notification.close();
-//     var url = 'https://google.com';
-//     event.waitUntil(
-//         clients.matchAll({
-//             type: 'window'
-//         })
-//         .then(function(windowClients) {
-//             for (var i = 0; i < windowClients.length; i++) {
-//                 var client = windowClients[i];
-//                 if (client.url === url && 'focus' in client) {
-//                     return client.focus();
-//                 }
-//             }
-//             if (clients.openWindow) {
-//                 return clients.openWindow(url);
-//             }
-//         })
-//     );
-// });
+  self.registration.showNotification(title, {
+    body: message,
+    // icon: 'images/icon.png',
+  });
+}
