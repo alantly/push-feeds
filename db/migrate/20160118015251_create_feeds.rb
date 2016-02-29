13dd868c9eb418d0cc2eb32b4d60af6a4a583d55
Alan <alanly211@gmail.com>
2016-02-28 17:44:26 -0800

    updated mispelling on string column

--- db/migrate/20160118015251_create_feeds.rb
+++ db/migrate/20160118015251_create_feeds.rb
@@ -1,7 +1,7 @@
 class CreateFeeds < ActiveRecord::Migration
   def change
     create_table :feeds do |t|
-      t.stribg :name, default: ""
+      t.string :name, default: ""
       t.string :url, :unique => true, index: true
       t.string :secret, :unique => true
 
