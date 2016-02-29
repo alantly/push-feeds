0938f3d3b3a7a2a083e35f47ce8a5ff6e754251c
Alan <alanly211@gmail.com>
2016-02-28 18:22:21 -0800

    updated notification title, added more todo

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
     resp = { title: "Push-Feeds Notification", message: "No New Updates!", url: "https://"+ENV['hostname']}
     if current_user
       top_notification = current_user.notifications.first
-      response = { title: "New PF Notification!", message: top_notification.title, url: top_notification.url }
+      response = { title: "New Push-Feeds Notification!", message: top_notification.title, url: top_notification.url }
       top_notification.destroy!
     end
     render json: response
