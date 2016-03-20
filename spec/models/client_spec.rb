ab7c9ec5cbccd378eab11a3958ebae01a1d0471b
Alan <alanly211@gmail.com>
2016-03-19 18:05:50 -0700

    adding factory girl and simple cov

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "instance methods" do
+    it "should notify push service using self subscription id" do
+
+    end
+  end
 end
