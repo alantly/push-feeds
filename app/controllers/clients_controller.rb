8545af8ac5ae14f988a1f9b41cc371f805c718b5
Alan <alanly211@gmail.com>
2016-04-17 22:14:10 -0700

    updat notifications with a default msg on empty payload

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -23,7 +23,7 @@ class ClientsController < ApplicationController
 
   def send_notification
     @client = Client.find_by_endpoint(params.require(:endpoint))
-    # contact nodejs server
+    @client.notify
     head :no_content
   end
 
