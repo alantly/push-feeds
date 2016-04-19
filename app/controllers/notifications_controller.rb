fa76755f7fae342f406ec4157cdb843b371bbc81
Alan <alanly211@gmail.com>
2016-04-18 17:53:57 -0700

    pushing to push-api. removed creating notification objects

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,38 +1,18 @@
 class NotificationsController < ApplicationController
-  skip_before_filter :authenticate_user!, :only => :recent
-
-  def recent
-    response = { title: "Push-Feeds Notification", message: "No New Updates!", \
-      url: "https://"+ENV['hostname'] }
-    client = Client.find_by_endpoint(client_params)
-    if client and not client.notifications.empty?
-      @top_notification = client.notifications.first
-      response = { title: "New Push-Feeds Notification!", message: @top_notification.title, \
-        url: @top_notification.url }
-      @top_notification.destroy
-    end
-    render json: response
-  end
 
   def self.create_notification feed_id, body
     feed = Feed.find_by_id(feed_id)
     superfeedr_update = JSON.parse(body)
     superfeedr_update["items"].each do |item|
-      notification = Notification.find_by_site_id item["id"]
-      if notification.nil?
-        feed.clients.each do |client|
-          Notification.create site_id: item["id"], \
-            title: item["title"], url: item["permalinkUrl"], client: client
-        end
-        feed.push_feed_to_users
+      notification = {
+        title: "New Push-Feeds Notification!",
+        message: notification[:message],
+        url: notification[:url],
+      }
+      feed.clients.each do |client|
+        client.push notification
       end
     end
-  end
-
-  private
-
-  def client_params
-    params.permit(:endpoint).require(:endpoint)
   end
 
 end
