cf70978a6b396032408fcfc6bccf7b32487313c1
Alan <alanly211@gmail.com>
2016-06-25 16:31:00 -0700

    clear erros on route leave

--- app/assets/javascripts/components/layout/app.js
+++ app/assets/javascripts/components/layout/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import NavigationBar from './navigationBar';
+import ErrorAlert from '../error/errorAlert';
 
-export default function App({ children, location }) {
+function App({ children, location, errors }) {
   return (
     <div>
       <NavigationBar {...location} />
+      <ErrorAlert errors={errors} />
       {children}
     </div>
   );
@@ -13,4 +16,13 @@ export default function App({ children, location }) {
 App.propTypes = {
   children: React.PropTypes.object,
   location: React.PropTypes.object,
+  errors: React.PropTypes.array,
 };
+
+function mapStateToProps(state) {
+  return {
+    errors: state.serverError.errors,
+  };
+}
+
+export default connect(mapStateToProps)(App);
