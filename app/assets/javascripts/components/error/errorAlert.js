508b71a172a46a3cf63de04f9426a0a51db0a980
Alan <alanly211@gmail.com>
2016-06-30 08:44:52 -0700

    added clear errors button

--- app/assets/javascripts/components/error/errorAlert.js
+++ app/assets/javascripts/components/error/errorAlert.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearErrors } from '../../actions/serverError';
 
-export default function ErrorAlert({ errors }) {
+function ErrorAlert({ errors, dispatch }) {
   return (
-    <div>
-    {errors.length !== 0?
-      <div className="alert alert-danger">
-        <ul style={{ listStyleType: 'circle' }}>
-          {errors.map(error =>
-            <li key={error.key} >
-              {error.msg}
-            </li>
-          )}
-        </ul>
-      </div> : false
-    }
+    <div className="container">
+      {errors.length !== 0?
+        <div className="alert alert-danger">
+          <div className="row">
+            <div className="col-md-offset-11 col-md-1 text-right">
+              <i
+                id="error-btn"
+                className="fa fa-remove fa-lg"
+                aria-hidden="true"
+                onClick={() => dispatch(clearErrors())}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <ul style={{ listStyleType: 'circle' }}>
+              {errors.map(error =>
+                <li key={error.key}>
+                  {error.msg}
+                </li>
+              )}
+            </ul>
+          </div>
+        </div> : false
+      }
     </div>
   );
 }
 
 ErrorAlert.propTypes = {
   errors: React.PropTypes.array.isRequired,
+  dispatch: React.PropTypes.func.isRequired,
 };
+
+export default connect()(ErrorAlert);
